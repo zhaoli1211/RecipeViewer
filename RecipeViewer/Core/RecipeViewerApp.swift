0b549bf2125bb49a8ddeca2db991963f091bd63d
zhaoli1211 <zhaoli841211@gmail.com>
2025-02-21 21:42:11 -0800

    RecipeViewer

---
+++ RecipeViewer/Core/RecipeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeViewerApp.swift
+//  RecipeViewer
+//
+//  Created by 赵立 on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
